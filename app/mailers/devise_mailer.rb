f4e9403daa8f0af0bf5af28984ee7909653a0b98
Eric Garside <garside@gmail.com>
2017-01-02 15:05:06 -0500

    Fixing subject line

--- app/mailers/devise_mailer.rb
+++ app/mailers/devise_mailer.rb
@@ -2,9 +2,9 @@ class DeviseMailer < Devise::Mailer
   def reset_password_instructions(record, token, opts={})
     mail = super
     if record.password_set?
-      mail.subject = 'Reset your soul.camp password!'
+      mail.subject = 'Reset your patriciamoreno.com password!'
     else
-      mail.subject = 'Create your soul.camp account!'
+      mail.subject = 'Create your patriciamoreno.com account!'
     end
     mail
   end
